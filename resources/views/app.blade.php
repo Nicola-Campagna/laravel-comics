52e940ff45dc67dd4a7291f401db2991ec9f0fb0
Nicola-Campagna <campagnanicola379@gmail.com>
2023-03-31 12:18:28 +0200

    add images+header

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>LARAVEL COMICS </title>
-      {{-- Includiamo gli assets con la direttiva @vite --}}
-  @vite('resources/js/app.js')
+    <title>LARAVEL COMICS</title>
+
+    @vite('resources/js/app.js')
 </head>
 <body>
-    
+    @include('partials/_header')
 </body>
 </html>
---
+++ resources/views/partials/_header.blade.php
@@ -0,0 +1,56 @@
+<header>
+
+    <div class="container-fluid">
+      <div class="row">
+          <div class="col">
+              <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                  <a class="navbar-brand" href="#">
+                      <img src="{{Vite::asset('resources/img/images/dc-logo.png')}}" alt="logo" class="img-fluid">
+                  </a>
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">CHARACTERS {{--<span class="sr-only">(current)</span></a>--}}
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">CONTACTS</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">MOVIES</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">TV</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">GAMES</a>
+                        </li> 
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">COLLECTIONABLES</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">VIDEOS</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">NEWS</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">SHOP</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">FANS</a>
+                        </li>
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                  </div>
+                </nav>
+          </div>
+      </div>
+  </div>
+</header>
